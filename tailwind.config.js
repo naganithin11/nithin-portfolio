6a7091c6c0640ccfba61df60a2ed1792181b706e
naganithin11 <nithindigitalservices@gmail.com>
2025-05-25 00:52:33 +0530

    Deploy portfolio: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'Schibsted': ['Schibsted Grotesk', 'sans-serif'],
+        'Outfit': ['Outfit', 'sans-serif'],
+        'Ovo': ['Ovo', 'serif'],
+      },
+      colors: {
+        primary: 'var(--primary-color)',
+      }
+    }
+  }
+};
